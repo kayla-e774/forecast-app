96bfd8a9fabb0247ecf464f7b3b8cfe71d428806
Kayla E <kengelstad16@gmail.com>
2025-01-27 09:39:32 -0600

    show forecast functionality

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -2,18 +2,25 @@ import { Router } from 'express';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // TODO: GET weather data from city name
+  WeatherService.setCity(req.body.cityName);
+  try {
+    const allWeather = await WeatherService.buildWeateherData();
+    return res.status(200).json(allWeather);
+  } catch(error) {
+    return res.status(400).send("Bad Request, invalid city.")
+  }
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req, _res) => {});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (_req, _res) => {});
 
 export default router;
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,31 +1,144 @@
+import dayjs, { type Dayjs } from 'dayjs';
 import dotenv from 'dotenv';
+// import { json } from 'stream/consumers';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-
+/*
+interface Coordinates {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
+*/
 // TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: Dayjs | string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+  icon: string;
+  iconDescription: string;
+  constructor(
+    city: string,
+    date: Dayjs | string,
+    tempF: number,
+    windSpeed: number,
+    humidity: number,
+    icon: string,
+    iconDescription: string
+  ) {
+    this.city = city;
+    this.date = date;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
-  // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
-  // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
-  // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
-  // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
-  // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
-  // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
-  // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
-  // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  
+  private baseURL?: string;
+
+  private apiKey?: string;
+
+  lat?: number;
+
+  lon?: number;
+
+  private city = '';
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+
+    this.apiKey = process.env.API_KEY || '';
+  }
+
+  setCity(city: string): void {
+    this.city = city;
+  }
+
+  // TODO: method to fetch weather based on city 
+  fetchCityWeather = async () => {
+    const weatherResponse = await fetch(
+      `${this.baseURL}/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=imperial`
+    );
+
+    const weatherBody = await weatherResponse.json();
+    const currentWeather = new Weather(
+      weatherBody.name,
+      dayjs().format('MM/DD/YYYY'), 
+      weatherBody.main.temp, 
+      weatherBody.wind.speed,
+      weatherBody.main.humidity,
+      weatherBody.weather[0].icon,
+      weatherBody.weather[0].description);
+    
+    this.lon = weatherBody.coord.lon;
+    this.lat = weatherBody.coord.lat;
+
+    return currentWeather;
+  }
+
+  makeWeather = (weatherJson: any, index: number) => {
+    const weather = new Weather(
+      weatherJson.city.name,
+      weatherJson.list[index].dt_txt,
+      weatherJson.list[index].main.temp,
+      weatherJson.list[index].wind.speed,
+      weatherJson.list[index].main.humidity,
+      weatherJson.list[index].weather[0].icon,
+      weatherJson.list[index].weather[0].description
+    )
+
+    return weather;
+  }
+
+  // TODO: method to fetch 5 day forecast based on lon and lat 
+  fetchForecast = async () => {
+    const forecastWeather = new Array<Weather>;
+    const forecastResponse = await fetch(
+      `${this.baseURL}/data/2.5/forecast?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=imperial`
+    );
+
+    const forecastBody = await forecastResponse.json();
+
+    const forecastItem8 = this.makeWeather(forecastBody, 7);
+    forecastWeather.push(forecastItem8);
+
+    const forecastItem16 = this.makeWeather(forecastBody, 15);
+    forecastWeather.push(forecastItem16);
+
+    const forecastItem24 = this.makeWeather(forecastBody, 23);
+    forecastWeather.push(forecastItem24);
+
+    const forecastItem32 = this.makeWeather(forecastBody, 31);
+    forecastWeather.push(forecastItem32);
+
+    const forecastItem40 = this.makeWeather(forecastBody, 39);
+    forecastWeather.push(forecastItem40);
+
+    return forecastWeather;
+  }
+
+  // TODO: method to build and return your array of weather objects
+  buildWeateherData = async () => {
+    const allWeather = new Array<Weather>;
+    const todaysWeather = await this.fetchCityWeather();
+
+    allWeather.push(todaysWeather);
+
+    const forecast: Array<Weather> = await this.fetchForecast();
+    const fullWeather = allWeather.concat(forecast);
+
+    return fullWeather;
+  }
 }
 
 export default new WeatherService();
