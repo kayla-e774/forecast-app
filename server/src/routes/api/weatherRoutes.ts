0a95e943fcdd48391175ea4b2f6bf939c7f20102
Kayla E <kengelstad16@gmail.com>
2025-01-27 21:02:03 -0600

    delete capability added

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -32,6 +32,17 @@ router.get('/history', async (_req, res) => {
 });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (_req, _res) => {});
+router.delete('/history/:id', async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({ msg: 'City id is required' });
+    }
+    await HistoryService.removeCity(req.params.id);
+    res.json({ success: 'City successfully removed from search history' });
+  } catch(err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 export default router;
