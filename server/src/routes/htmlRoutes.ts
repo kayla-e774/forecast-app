e509ddd1451f8343835fe9d443ed9d3bc4e7e1db
Kayla E <kengelstad16@gmail.com>
2025-01-27 20:04:01 -0600

    search history saved to file

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -8,7 +8,11 @@ const router = Router();
 
 // TODO: Define route to serve index.html
 router.get('/', async (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
+
+router.get('*', async (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
 
 export default router;
